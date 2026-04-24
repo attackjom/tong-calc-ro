2154c7598033d3ec2f48fde30af77718f8f4c7af
Joam <35770095+attackjom@users.noreply.github.com>
2026-04-24 15:27:24 +0700

    End

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,9 +2,9 @@ import { EnvironmentModel } from './environment.model';
 
 export const environment: EnvironmentModel = {
   production: true,
-  surveyUrl: 'https://forms.gle/TRDtvj5MEK2gWsE29',
+  surveyUrl: 'https://valkyrie2.github.io/ro-calculator',
   issueTrackingUrl:
-    'https://docs.google.com/spreadsheets/d/1Zq9dkejS4d-kjUQ1kbSAmR0im7Hw5onfGEs7gLZ3vY4/edit?usp=sharing',
+    'https://valkyrie2.github.io/ro-calculator/#/',
   roBackendUrl: 'https://ro-calc.luminotus.com',
   youtubeVideoUrl: 'https://www.youtube.com/watch?v=ec5U-ZxvoFM&list=PLJi-aEFx61gw97he4dSOWP5-fADXegbyH&index=1&t',
 };
